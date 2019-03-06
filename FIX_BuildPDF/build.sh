404afd1d719b59c2a7600b83b5ed4583f8c822e9
Swissky <swisskysec@protonmail.com>
2019-03-07 00:07:55 +0100

    Fix name's capitalization

--- FIX_BuildPDF/build.sh
+++
@@ -1,9 +0,0 @@
-# GitPrint from Payload
-
-find .  -name "*.md" | sed "s/\.\///g" | sort | xargs -I{} wget --content-disposition "https://gitprint.com/swisskyrepo/PayloadsAllTheThings/blob/master/"{}"?download"
-pdfjoin *.pdf 
-
-
-# NOTE : 
-# check for 502 errors from gitprint
-# XSS and Mimikatz don't work with Gitprint ;.
